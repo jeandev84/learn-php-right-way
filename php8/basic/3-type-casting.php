f745ef313e9034ab8736626b92715faccb6e2455
Jean Claude <jeanyao@ymail.com>
2023-12-13 00:01:22 +0500

    Type casting

---
+++ php8/basic/3-type-casting.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Data Types & Type Casting */
+
+# 4 Scalar Types
+    # bool
+    # int
+    # float
+    # string
+
+# 4 Compound Types
+    # array
+    # object
+    # callable
+    # iterable
+
+# 2 Special Types
+    # resource
+    # null
