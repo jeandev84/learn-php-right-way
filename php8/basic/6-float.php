e4f03790f90faa1e5538855a2e22bafdc7d54061
Jean Claude <jeanyao@ymail.com>
2023-12-13 01:08:00 +0500

    Float

--- php8/basic/6-float.php
+++ php8/basic/6-float.php
@@ -13,5 +13,16 @@ var_dump($a, $b, $c, $d);
 echo PHP_FLOAT_MAX;
 echo PHP_FLOAT_MIN;
 
-$x = floor((0.1 + 0.7) * 10);
-echo $x;
+$floor1 = floor((0.1 + 0.7) * 10); // 7
+echo $floor1;
+
+$floor2 = floor(7.9999999999911118); // 7
+echo $floor2;
+
+$ceil1 = ceil((0.1 + 0.7) * 10); // 8
+echo $ceil1;
+
+$ceil2 = ceil((0.1 + 0.2) * 10); // 4
+echo $ceil2;
+
+
