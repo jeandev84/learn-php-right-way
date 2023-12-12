664d4d038abd3218608e48a8eb595b37bea686e7
Jean Claude <jeanyao@ymail.com>
2023-12-13 04:03:40 +0500

    Loops

---
+++ php8/basic/15-switch-statement.php
@@ -0,0 +1,10 @@
+<?php
+
+$paymentStatus = 'paid';
+
+switch ($paymentStatus):
+    case 'paid': echo 'Paid'; break;
+    case 'declined': echo 'Payment Declined'; break;
+    case 'pending': echo 'Pending Declined'; break;
+    default: echo 'Unknown Payment Status'; break;
+endswitch;
