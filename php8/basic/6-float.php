6e9022c950d612983a99ad61ccee998733eaca69
Jean Claude <jeanyao@ymail.com>
2023-12-13 01:11:05 +0500

    Float

--- php8/basic/6-float.php
+++ php8/basic/6-float.php
@@ -26,3 +26,19 @@ $ceil2 = ceil((0.1 + 0.2) * 10); // 4
 echo $ceil2;
 
 
+$x = 0.23;
+$y = 1 -0.77;
+
+var_dump($x, $y);
+echo ($x === $y) ? 'Yes' : 'No';
+
+$ceil3 = ceil((0.1 + 0.2) * 10);
+echo $ceil3;
+
+
+echo NAN; // Not A Number
+echo log(-1);
+echo INF;
+# echo PHP_FLOAT_MAX * 2;
+
+
