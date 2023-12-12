7f14c367bb2abe7d719717c6d5573af76c15527b
Jean Claude <jeanyao@ymail.com>
2023-12-13 02:27:17 +0500

    Operators

---
+++ php8/basic/11-operators.php
@@ -0,0 +1,52 @@
+<?php
+
+/* OPERATORS */
+
+// Arithmetic Operators (+ - * / % **)
+$x = 10;
+$y = 2;
+
+var_dump($x + $y);
+var_dump($x - $y);
+var_dump($x * $y);
+var_dump($x / $y);
+var_dump(fdiv($x, $y));
+var_dump($x % $y);
+var_dump(fmod($x, $y));
+var_dump($x ** $y);
+
+// Assignment Operators (= += -= *= /= %= **=)
+
+
+// String Operators (. .=)
+
+
+// Comparison Operators (== === =/=  <> ==/= < > <= >= <=> ?? ?:)
+
+
+
+// Error Control Operators (@)
+
+
+// Increment/Decrement Operators (++, --)
+
+
+
+// Logical Operators (&& || ! and or xor)
+
+
+
+// Bitwise Operators (& | ^ ~ << >>)
+
+
+
+// Array Operators (+ == === =/= <> ==/=)
+
+
+// Execution Operators (``)
+
+
+// Type Operators (instanceof)
+
+
+// Nullsafe Operator - PHP8 (?)
