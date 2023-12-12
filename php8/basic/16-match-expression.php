270ee0fa79bf470b95e89227c593ee1b735db2ee
Jean Claude <jeanyao@ymail.com>
2023-12-13 04:23:39 +0500

    Match expression

--- php8/basic/16-match-expression.php
+++ php8/basic/16-match-expression.php
@@ -27,7 +27,8 @@ echo '<hr/>';
 $paymentStatusDisplay = match ($paymentStatus) {
    1 => 'Paid',
    2,3 => 'Payment Declined',
-   0 => 'Pending Payment'
+   0 => 'Pending Payment',
+   default => 'Unknown Payment Status'
 };
 
 echo $paymentStatusDisplay;
