89093e1979e7f817517f4e953e9b23b8b14a6916
Jean Claude <jeanyao@ymail.com>
2023-12-13 04:44:45 +0500

    Include files

---
+++ php8/basic/18-include-files.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'Home';
+require __DIR__.'/pages/home.php';
+
+/*
+require_once __DIR__.'/pages/home.php';
+include __DIR__.'/pages/home.php';
+include_once __DIR__.'/pages/home.php';
+*/
+
