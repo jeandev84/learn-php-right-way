38317b1523dc5cdf2359907a28c25bb238751dc4
Jean Claude <jeanyao@ymail.com>
2023-12-13 05:06:58 +0500

    Functions

--- php8/basic/19-functions.php
+++ php8/basic/19-functions.php
@@ -2,15 +2,18 @@
 
 /** Functions */
 
-foo();
-bar();
 
-function foo() {
-    echo 'Foo';
-    function bar() {
-        echo 'Bar';
-        function foo() {
-            echo 'Foo 2';
-        }
+function multi(int|float $x, int|float $y = 10): int|float {
+    if ($x % 2 === 0) {
+        $x /= 2;
     }
+    return $x * $y;
 }
+
+echo multi(5.0, 10);
+
+function sum(...$numbers): int|float {
+    return array_sum($numbers);
+}
+
+echo sum(6.0, 7, 50, 100, 25, 8, 9);
