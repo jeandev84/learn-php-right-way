5a949a5a89ace26e4322824360ab492705fa18cc
Jean Claude <jeanyao@ymail.com>
2023-12-13 16:13:55 +0500

    Namespace

--- php8/intermediate/www/src/PaymentGateway/Paddle/Transaction.php
+++ php8/intermediate/www/src/PaymentGateway/Paddle/Transaction.php
@@ -1,7 +1,15 @@
 <?php
 namespace PaymentGateway\Paddle;
 
+use DateTime;
+use Notification\Email;
+
 class Transaction
 {
-
+     public function __construct()
+     {
+         # var_dump(new CustomerProfile());
+         # var_dump(new DateTime());
+         var_dump(new Email());
+     }
 }
--- php8/intermediate/www/src/public/index.php
+++ php8/intermediate/www/src/public/index.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
 require_once '../PaymentGateway/Paddle/Transaction.php';
+require_once '../Notification/Email.php';
 
 
-var_dump(new \PaymentGateway\Stripe\Transaction());
+
+var_dump(new \PaymentGateway\Paddle\Transaction());
 
 
