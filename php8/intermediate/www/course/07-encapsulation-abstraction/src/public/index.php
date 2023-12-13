a083d1252b0365757adbc62384ccae85226ac3fd
Jean Claude <jeanyao@ymail.com>
2023-12-13 20:05:18 +0500

    Encapsulation and Abstraction

---
+++ php8/intermediate/www/course/07-encapsulation-abstraction/src/public/index.php
@@ -0,0 +1,24 @@
+<?php
+use App\PaymentGateway\Paddle\Transaction;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+/*
+$transaction = new Transaction(25);
+
+$reflectionProperty = new ReflectionProperty($transaction, 'amount');
+$reflectionProperty->setAccessible(true);
+
+$reflectionProperty->setValue($transaction, 125);
+
+dump($reflectionProperty->getValue($transaction));
+
+$transaction->process();
+*/
+
+$transaction = new Transaction(25);
+
+$transaction->copyFrom(new Transaction(100));
+# $transaction->process();
+
+
