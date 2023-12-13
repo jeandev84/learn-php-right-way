4a3f63043a1f625c5282cdf70b17cf177d3e7442
Jean Claude <jeanyao@ymail.com>
2023-12-13 16:29:19 +0500

    Namespace

---
+++ php8/intermediate/www/course/03-namespace/src/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,17 @@
+<?php
+namespace PaymentGateway\Paddle;
+
+use DateTime;
+use Notification\Email;
+
+class Transaction
+{
+     public function __construct()
+     {
+         # var_dump(new CustomerProfile());
+         # var_dump(new DateTime());
+         # var_dump(new Email());
+         var_dump(explode(',', 'some,text,separated,by,comma'));
+     }
+}
+
