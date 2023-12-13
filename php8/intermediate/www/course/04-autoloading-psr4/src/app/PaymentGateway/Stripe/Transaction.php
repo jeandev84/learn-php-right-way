2577b67a1442a020cd636e830f9753f69f23daf2
Jean Claude <jeanyao@ymail.com>
2023-12-13 18:01:39 +0500

    Coding Standards and PSR-4

---
+++ php8/intermediate/www/course/04-autoloading-psr4/src/app/PaymentGateway/Stripe/Transaction.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\PaymentGateway\Stripe;
+
+
+/**
+ * Class Transaction
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @package
+*/
+class Transaction
+{
+
+}
