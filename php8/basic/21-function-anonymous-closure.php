3a483b6bc8fe22e8e11200c185c26c84b57e35eb
Jean Claude <jeanyao@ymail.com>
2023-12-13 06:03:50 +0500

    Function anonymous, Callable, Arrow functions

--- php8/basic/21-function-anonymous-closure.php
+++ php8/basic/21-function-anonymous-closure.php
@@ -2,11 +2,7 @@
 
 /** Variable, anonymous & arrow functions */
 
-
-/**
- * @param int|float ...$numbers
- * @return int|float
-*/
+/*
 function sum(int|float ...$numbers): int|float {
     return array_sum($numbers);
 }
@@ -20,10 +16,10 @@ if (is_callable($func)) {
     echo 'Not Callable';
 }
 
-$x =1;
+$callback =1;
 // anonymous function
-$sum = function (int|float ...$numbers) use ($x): int|float {
-    echo $x;
+$sum = function (int|float ...$numbers) use ($callback): int|float {
+    echo $callback;
     return array_sum($numbers);
 };
 
@@ -41,3 +37,26 @@ print_r($array1);
 print_r($array2);
 echo '</pre>';
 
+
+$items = [1, 2, 3, 4];
+$callback = function ($element) {
+    return $element * 2;
+};
+
+$callback = array_map($callback, $items);
+echo '<pre>';
+print_r($array1);
+
+print_r($array2);
+echo '</pre>';
+*/
+
+$sum = function (callable $callback, int|float ...$numbers): int|float {
+    return $callback(array_sum($numbers));
+};
+
+function doubleElement($element): int|float {
+    return $element * 2;
+}
+
+echo $sum('doubleElement', 1, 2, 3, 4);
