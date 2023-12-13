5600027d313a6cdce11deeb9671412615f78c0db
Jean Claude <jeanyao@ymail.com>
2023-12-13 20:32:38 +0500

    Rename file

--- php8/intermediate/www/src/app/Toasters/ToasterPro.php
+++ php8/intermediate/www/src/app/Toasters/ToasterPro.php
@@ -6,7 +6,16 @@ class ToasterPro extends Toaster
 {
     protected int $size = 4;
 
-    public function toastBagel()
+
+    public function addSlice(string $slice): void
+    {
+        parent::addSlice($slice);
+    }
+
+
+
+
+    public function toastBagel(): void
     {
         foreach ($this->slices as $i => $slice) {
             echo ($i + 1). ': Toasting '. $slice . ' with bagels option'. PHP_EOL;
