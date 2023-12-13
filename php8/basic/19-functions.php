6bedd3f44a5f4d828ef58bb22d2e05bdef0624fb
Jean Claude <jeanyao@ymail.com>
2023-12-13 05:18:52 +0500

    Functions Parameters

--- php8/basic/19-functions.php
+++ php8/basic/19-functions.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 /** Functions */
 
 /*
@@ -18,19 +18,35 @@ function sum(...$numbers): int|float {
 
 echo sum(6.0, 7, 50, 100, 25, 8, 9);
 
-*/
 
 function sum(int|float $x, int|float $y, int|float ...$numbers): int|float {
     return $x + $y + array_sum($numbers);
 }
 
-/*
+
 $a = 6.0;
 $b = 7;
 echo sum($a, $b, 50, 100, 25, 8, 9);
-*/
 
 $a       = 6.0;
 $b       = 7;
 $numbers = [50, 100, 25, 8, 9];
 echo sum($a, $b, ...$numbers);
+
+*/
+
+
+function foo(int $x, int $y): int {
+    if ($x % $y === 0) {
+        return $x / $y;
+    }
+    return $x;
+}
+
+$x = 6;
+$y = 3;
+
+# setcookie(name: 'foo', value: 'bar', httponly: true);
+
+echo foo(x: $x, y: $y);
+
