d4d14aad63f59cedbb79b183892b30cab9054255
Jean Claude <jeanyao@ymail.com>
2023-12-13 16:21:12 +0500

    Namespace

--- php8/intermediate/www/src/public/index.php
+++ php8/intermediate/www/src/public/index.php
@@ -5,9 +5,14 @@ require_once '../Notification/Email.php';
 require_once '../PaymentGateway/Paddle/CustomerProfile.php';
 require_once '../PaymentGateway/Paddle/Transaction.php';
 
+# var_dump(new \PaymentGateway\Paddle\Transaction());
+
+use PaymentGateway\Paddle\Transaction;
+use PaymentGateway\Stripe\Transaction as StripeTransaction;
+
+$paddleTransaction = new Transaction();
+$stripeTransaction = new StripeTransaction();
 
 
-
-var_dump(new \PaymentGateway\Paddle\Transaction());
 
 
