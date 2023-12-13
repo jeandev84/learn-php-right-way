527d524eb52c7c3a198ca1c5711f4b6f90b83fa0
Jean Claude <jeanyao@ymail.com>
2023-12-13 07:53:19 +0500

    PHP Ini

--- php8/basic/php-ini.php
+++ php8/basic/php-ini.php
@@ -7,6 +7,7 @@ ini_get();
 ini_set();
 */
 // error_reporting, error_log, display_errors
+/*
 var_dump(ini_get('error_reporting'));
 var_dump(E_ALL);
 var_dump(E_DEPRECATED);
@@ -14,11 +15,37 @@ var_dump(E_DEPRECATED);
 ini_set('error_reporting', 0);
 ini_set('error_reporting', E_ALL && ~E_WARNING);
 
-
 var_dump(ini_get('display_errors'));
 ini_set('display_errors', 0);
 ini_set('display_errors', 1);
 
+
+ini_set('max_execution_time', 3);
+
+sleep(5);
+
+echo "Hello World";
+*/
+
+
+var_dump(ini_get('memory_limit'));
+ini_set('max_execution_time', 3);
+
+sleep(5);
+
+echo "Hello World";
+
+
+// MAX MEMORY LIMIT ERROR
+var_dump(ini_get('memory_limit'));
+
+
+# .... INF
+$string = 'X';
+for ($i = 0; $i < 1000; $i++):
+    $string .= $string;
+endfor;
+echo $string;
 
 
 
