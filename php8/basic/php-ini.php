eac6ce8b5e66b6a5b3161f8172ff01c33a9043fc
Jean Claude <jeanyao@ymail.com>
2023-12-13 07:46:38 +0500

    PHP Ini

---
+++ php8/basic/php-ini.php
@@ -0,0 +1,18 @@
+<?php
+
+/* php.ini & configuration */
+/* https://www.php.net/manual/en/ini.list.php */
+/*
+ini_get();
+ini_set();
+*/
+// error_reporting, error_log, display_errors
+var_dump(ini_get('error_reporting'));
+var_dump(E_ALL);
+var_dump(E_DEPRECATED);
+
+ini_set('error_reporting', 0);
+ini_set('error_reporting', E_ALL && ~E_WARNING);
+
+
+
