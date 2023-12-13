cf19577073da9ec6ecd883eda20825d2b1fcf710
Jean Claude <jeanyao@ymail.com>
2023-12-13 05:32:19 +0500

    Functions Parameters

--- php8/basic/20-variable-scopes.php
+++ php8/basic/20-variable-scopes.php
@@ -21,3 +21,23 @@ function func(): void {
 
 foo();
 echo $x;
+
+$x = 5;
+
+
+function getValue(): string {
+    static $value = null;
+    if ($value === null) {
+        $value = someVeryExpensiveFunction();
+    }
+    return $value;
+}
+
+
+function someVeryExpensiveFunction(): string {
+    sleep(2);
+    echo 'Processing';
+    return 10;
+}
+
+echo getValue(), "<br/>";
