6ac87c2d4e7de27fea3cbfec64d9b71c363abdfa
Jean Claude <jeanyao@ymail.com>
2023-12-13 05:44:09 +0500

    Function anonymous, Callable, Arrow functions

--- php8/basic/21-function-anonymous-closure.php
+++ php8/basic/21-function-anonymous-closure.php
@@ -30,4 +30,14 @@ $sum = function (int|float ...$numbers) use ($x): int|float {
 echo $sum(1, 2, 3, 4);
 
 
+// callback function
+$array1 = [1, 2, 3, 4];
+$array2 = array_map(function ($element) {
+     return $element * 2;
+}, $array1);
+echo '<pre>';
+print_r($array1);
+
+print_r($array2);
+echo '</pre>';
 
