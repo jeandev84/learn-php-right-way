feb39d7590d8a149603126c0c59c72829f89a7d0
Jean Claude <jeanyao@ymail.com>
2023-12-13 08:50:50 +0500

    Filesystem

--- php8/basic/26-filesystem.php
+++ php8/basic/26-filesystem.php
@@ -10,10 +10,67 @@ mkdir('foo/bar', recursive: true);
 rmdir('foo/bar');
 */
 
+
+// Cache
+/*
 if (! file_exists('foo.txt')) {
     touch('foo.txt');
 }
 
-echo filesize('too.txt');
+echo filesize('foo.txt');
 file_put_contents('foo.txt', 'hello world');
+
+clearstatcache();
 echo filesize('too.txt');
+*/
+
+
+function writeTo(string $filename, string $content): void {
+    if (! file_exists($filename)) {
+        touch($filename);
+    }
+
+    echo filesize($filename);
+    file_put_contents($filename, $content);
+    clearstatcache();
+    echo filesize($filename);
+}
+
+
+// Open file
+function read(string $filename): void {
+    if (! file_exists($filename)) {
+        echo 'File not found';
+        return;
+    }
+
+    $file = fopen($filename, 'r');
+
+    while (($line = fgets($file)) !== false) {
+        echo ($line). '<br/>';
+    }
+
+    fclose($file);
+}
+
+/* writeTo(__DIR__.'/data/file.txt', 'hello world'); */
+
+
+function readCsv(string $filename, string $mode = 'r'): void {
+    if (! file_exists($filename)) {
+        echo 'File not found';
+        return;
+    }
+
+    $file = fopen($filename, $mode);
+
+    while (($line = fgetcsv($file)) !== false) {
+        print_r($line);
+    }
+
+    fclose($file);
+}
+
+readCsv(__DIR__.'/data/file.txt');
+
+
