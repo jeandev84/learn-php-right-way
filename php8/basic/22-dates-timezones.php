874e0136c21d8d101c05726fc92ffcdacab83633
Jean Claude <jeanyao@ymail.com>
2023-12-13 06:28:39 +0500

    Dates and Time Zones

--- php8/basic/22-dates-timezones.php
+++ php8/basic/22-dates-timezones.php
@@ -11,3 +11,25 @@ echo $currentTime - 60 * 60 * 24, '<br/>';
 echo date('m/d/Y g:ia');
 echo date('m/d/Y g:ia',  $currentTime + 5 * 24 * 60 * 60);
 echo date('m/d/Y g:ia', $currentTime - 60 * 60 * 24);
+
+// Current timezone
+echo date_default_timezone_get();
+
+// Set Timezone
+date_default_timezone_set('UTC');
+
+echo date('m/d/Y g:ia');
+echo date('m/d/Y g:ia',  $currentTime + 5 * 24 * 60 * 60);
+echo date('m/d/Y g:ia', $currentTime - 60 * 60 * 24);
+
+
+date_default_timezone_set('Europe/Moscow');
+
+echo date('m/d/Y g:ia');
+echo date('m/d/Y g:ia',  $currentTime + 5 * 24 * 60 * 60);
+echo date('m/d/Y g:ia', $currentTime - 60 * 60 * 24);
+
+
+// Make time and format
+$time = mktime(0, 0, 0, 4, 10, null);
+echo date('m/d/Y g:ia', $time);
