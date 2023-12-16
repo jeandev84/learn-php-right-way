de7f84a38ff35c323b5d284d5382aed5af482259
Jean Claude <jeanyao@ymail.com>
2023-12-16 22:38:49 +0500

    Enum

--- php-8/3-Advanced/project/app/PHP_8_1/Examples/Enum/demo.php
+++ php-8/3-Advanced/project/app/PHP_8_1/Examples/Enum/demo.php
@@ -4,9 +4,15 @@ use App\PHP_8_1\Examples\Enum\PaymentStatus;
 
 $payment = new \App\PHP_8_1\Examples\Enum\Payment();
 
+var_dump(PaymentStatus::PAID);
+
+$payment->updateStatus(PaymentStatus::DECLINED);
+
+echo $payment->status()->name . PHP_EOL;
+echo $payment->status()->text() . PHP_EOL;
+
 $payment->updateStatus(PaymentStatus::PAID);
 
 echo $payment->status()->name . PHP_EOL;
 echo $payment->status()->text() . PHP_EOL;
 
-var_dump(PaymentStatus::PAID);
