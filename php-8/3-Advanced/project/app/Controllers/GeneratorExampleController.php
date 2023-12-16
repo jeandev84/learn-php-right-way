e045337dd0d7ba4c234f2350890bc90ff221cc40
Jean Claude <jeanyao@ymail.com>
2023-12-16 06:27:51 +0500

    Generator Yield

--- php-8/3-Advanced/project/app/Controllers/GeneratorExampleController.php
+++ php-8/3-Advanced/project/app/Controllers/GeneratorExampleController.php
@@ -21,8 +21,12 @@ class GeneratorExampleController
           /* $this->generatorService->loopNumbersFor(1, 10); */
           /* dump($this->ticketModel->all()); */
 
-          foreach ($this->ticketModel->all() as $ticket) {
+          $tickets = $this->ticketModel->all();
+
+          foreach ($tickets as $ticket) {
               echo $ticket['id'] . ': '. substr($ticket['content'], 0, 15) . '<br/>';
           }
+
+          # $tickets->rewind();
       }
 }
