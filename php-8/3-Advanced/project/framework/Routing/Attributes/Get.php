d0f1845b346d127cb66fedb2bd407326186cd8a0
Jean Claude <jeanyao@ymail.com>
2023-12-17 01:42:23 +0500

    Attributes - Simple Router

--- php-8/3-Advanced/project/framework/Routing/Attributes/Get.php
+++ php-8/3-Advanced/project/framework/Routing/Attributes/Get.php
@@ -5,6 +5,7 @@ namespace Framework\Routing\Attributes;
 
 use Attribute;
 
+// #[Attribute(Attribute::TARGET_METHOD|Attribute::TARGET_CLASS)]
 #[Attribute(Attribute::TARGET_METHOD)]
 class Get extends Route
 {
