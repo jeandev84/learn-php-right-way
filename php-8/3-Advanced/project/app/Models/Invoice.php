d2b4810a7ef6f266c1057e4e7898dd55ffc812c1
Jean Claude <jeanyao@ymail.com>
2023-12-18 10:42:32 +0500

    Eloquent save data and relationship

--- php-8/3-Advanced/project/app/Models/Invoice.php
+++ php-8/3-Advanced/project/app/Models/Invoice.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Enums\InvoiceStatus;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+/**
+ * @property int $id
+ * @property string $invoice_number
+ * @property float $amount
+ * @property InvoiceStatus $status
+ * @property Carbon $created_at
+ * @property Carbon $due_date
+*/
 class Invoice extends Model
 {
        # const UPDATED_AT = 'created_date';
--- php-8/3-Advanced/project/app/Models/InvoiceItem.php
+++ php-8/3-Advanced/project/app/Models/InvoiceItem.php
@@ -3,9 +3,19 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\InvoiceStatus;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+
+/**
+ * @property int $id
+ * @property int $invoice_id
+ * @property string $description
+ * @property int $quantity
+ * @property float $unit_price
+*/
 class InvoiceItem extends Model
 {
       public $timestamps = false;
